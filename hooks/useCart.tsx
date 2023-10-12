5b23a1c1d1f6a6dff2d7ab9b6eff5387ba2ae686
Rajneesh Mishra <rajneesh.solulab@gmail.com>
2023-10-12 19:29:20 +0530

    add user part of app

---
+++ hooks/useCart.tsx
@@ -0,0 +1,171 @@
+import { CartProductType } from "@/app/product/[productid]/ProductDetails";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "react-hot-toast";
+
+type CartContextType = {
+  cartTotalQty: number;
+  cartTotalAmount:number;
+  cartProducts: CartProductType[] | null;
+  handleAddProductToCart: (product: CartProductType) => void;
+  handleRemoveProductFromCart: (product: CartProductType) => void;
+  handleCartQtyIncrease: (product: CartProductType) => void;
+  handleCartQtyDecrease: (product: CartProductType) => void;
+  handleClearCart: () => void;
+};
+
+interface Props {
+  [propName: string]: any;
+}
+
+export const CartContext = createContext<CartContextType | null>(null);
+
+export const CartContextProvider = (props: Props) => {
+  const [cartTotalQty, setCartTotalQty] = useState(0);
+  const [cartTotalAmount, setCartTotalAmount] = useState(0);
+  const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(
+    null
+  );
+
+  useEffect(() => {
+    const cartItems: any = localStorage.getItem("classicCutsCartItems");
+    const cProducts: CartProductType[] | null = JSON.parse(cartItems);
+    setCartProducts(cProducts);
+  }, []);
+
+  useEffect(() => {
+    const getTotals = () => {
+      if (cartProducts) {
+        const { total, qty } = cartProducts?.reduce(
+          (acc, item) => {
+            const itemToatal = item.price * item.quantity;
+            acc.total += itemToatal;
+            acc.qty += item.quantity;
+            return acc;
+          },
+          {
+            total: 0,
+            qty: 0,
+          }
+        );
+        setCartTotalQty(qty);
+        setCartTotalAmount(total);
+      }
+    };
+    getTotals();
+  }, [cartProducts]);
+  const handleAddProductToCart = useCallback((product: CartProductType) => {
+    setCartProducts((prev) => {
+      let updatedCart;
+
+      if (prev) {
+        updatedCart = [...prev, product];
+      } else {
+        updatedCart = [product];
+      }
+      toast.success("Product added to cart");
+      localStorage.setItem("classicCutsCartItems", JSON.stringify(updatedCart));
+      return updatedCart;
+    });
+  }, []);
+
+  const handleRemoveProductFromCart = useCallback(
+    (product: CartProductType) => {
+      if (cartProducts) {
+        const filteredProducts = cartProducts.filter((item) => {
+          return item.id !== product.id;
+        });
+
+        setCartProducts(filteredProducts);
+        toast.success("Product removed");
+        localStorage.setItem(
+          "classicCutsCartItems",
+          JSON.stringify(filteredProducts)
+        );
+      }
+    },
+    [cartProducts]
+  );
+
+  const handleCartQtyDecrease = useCallback(
+    (product: CartProductType) => {
+      let updatedCart;
+      if (product.quantity === 1) {
+        return toast.error("You have reached minimum limit");
+      }
+
+      if (cartProducts) {
+        updatedCart = [...cartProducts];
+        const existingIndex = cartProducts.findIndex(
+          (item) => item.id === product.id
+        );
+
+        if (existingIndex > -1) {
+          updatedCart[existingIndex].quantity -= 1;
+        }
+        setCartProducts(updatedCart);
+        localStorage.setItem(
+          "classicCutsCartItems",
+          JSON.stringify(updatedCart)
+        );
+      }
+    },
+    [cartProducts]
+  );
+  const handleCartQtyIncrease = useCallback(
+    (product: CartProductType) => {
+      let updatedCart;
+      if (product.quantity === 99) {
+        return toast.error("You have reached maximum limit");
+      }
+
+      if (cartProducts) {
+        updatedCart = [...cartProducts];
+        const existingIndex = cartProducts.findIndex(
+          (item) => item.id === product.id
+        );
+
+        if (existingIndex > -1) {
+          updatedCart[existingIndex].quantity += 1;
+        }
+        setCartProducts(updatedCart);
+        localStorage.setItem(
+          "classicCutsCartItems",
+          JSON.stringify(updatedCart)
+        );
+      }
+    },
+    [cartProducts]
+  );
+
+  const handleClearCart = useCallback(() => {
+    setCartProducts(null);
+    setCartTotalQty(0);
+    localStorage.setItem("classicCutsCartItems", JSON.stringify(null));
+  }, [cartProducts]);
+
+  const value = {
+    cartTotalQty,
+    cartTotalAmount,
+    cartProducts,
+    handleAddProductToCart,
+    handleRemoveProductFromCart,
+    handleCartQtyIncrease,
+    handleCartQtyDecrease,
+    handleClearCart,
+  };
+  return <CartContext.Provider value={value} {...props} />;
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (context == null) {
+    throw new Error("useCart must be used within a CartContextProvider");
+  }
+  return context;
+};
