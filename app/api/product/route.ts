9960ec37f09a5c2f3b1bcfa7fa75a19701dfb0e7
Faizan Mansuri <faizanmansuri.work@gmail.com>
2023-10-13 16:17:11 +0530

    crud admin

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,10 +1,29 @@
+import { NextResponse } from "next/server";
+
 import prisma from "@/libs/prismadb";
-import {NextResponse} from "next/server";
-import {getCurrentUser} from '@/actions/getCurrentUser';
 
-export async function POST(request: Request){
-    const body = await request.json();
-    const {name, email, password} = body;
+import getCurrentUser from "@/actions/getCurrentUser";
 
-    
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role === "ADMIN") {
+    return NextResponse.error();
+  }
+  const body = await request.json();
+  const { name, description, price, brand, category, inStock, images } = body;
+
+  const product = await prisma.product.create({
+    data: {
+      name,
+      description,
+      price: parseFloat(price),
+      brand,
+      category,
+      inStock,
+      images,
+    },
+  });
+
+  return NextResponse.json(product)
 }
