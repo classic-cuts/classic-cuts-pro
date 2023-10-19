62d9c2e449afac0288fc863522301f3a865ad91f
Faizan Mansuri <faizanmansuri.work@gmail.com>
2023-10-19 14:58:06 +0530

    navbar comp added

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -4,10 +4,12 @@ import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
 import { getCurrentUser } from "@/actions/getCurrentUser";
+import Categories from "./Categories";
+import SearchBar from "./SearchBar";
 
 const redDressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
-const NavBar = async() => {
+const NavBar = async () => {
   const currentUser = await getCurrentUser();
 
   return (
@@ -15,17 +17,23 @@ const NavBar = async() => {
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex items-center justify-between gap-3 md:gap-0">
-            <Link href="/" className={`${redDressed.className} font-bold text-2xl`}>
+            <Link
+              href="/"
+              className={`${redDressed.className} font-bold text-2xl`}
+            >
               Classic Cuts
             </Link>
-            <div className="hidden md:block">Search</div>
+            <div className="hidden md:block">
+              <SearchBar />
+            </div>
             <div className="flex items-center gap-8 md:gap-12">
-              <CartCount/>
-              <UserMenu currentUser={currentUser}/>
+              <CartCount />
+              <UserMenu currentUser={currentUser} />
             </div>
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
